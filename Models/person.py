10c086848e291a493e7a1b2cf3320792c44cb588
Pavitra Bhat <pavitranbhat232003@gmail.com>
2026-02-08 18:57:27 +0530

    Version 3: Implemented encapsulation for Person and Student

--- Models/person.py
+++ Models/person.py
@@ -1,8 +1,17 @@
 class Person:
-    def __init__(self,name,id):
+    def __init__(self, name, id):
         self.name = name
-        self.id = id
-        
+        self.__id = id   # private
+
+    def get_id(self):
+        return self.__id
+
+    def set_id(self, new_id):
+        if new_id:
+            self.__id = new_id
+        else:
+            print("Invalid ID")
+
     def display_details(self):
-        print("Name:",self.name)
-        print("ID:",self.id)
+        print("Name:", self.name)
+        print("ID:", self.__id)
--- Models/student.py
+++ Models/student.py
@@ -3,11 +3,17 @@ from models.person import Person
 class Student(Person):
     def __init__(self, name, student_id):
         super().__init__(name, student_id)
-        self.courses = []
+        self.__courses = []   # private variable
 
     def enroll_course(self, course):
-        self.courses.append(course)
+        if course not in self.__courses:
+            self.__courses.append(course)
+        else:
+            print("Course already enrolled")
+
+    def get_courses(self):
+        return self.__courses
 
     def display_details(self):
         super().display_details()
-        print("Courses:", self.courses)
+        print("Courses:", self.__courses)
