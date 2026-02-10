d9067969c8ad56e2d0d30fa71388d9f5a3e730b5
Pavitra Bhat <pavitranbhat232003@gmail.com>
2026-02-10 19:35:28 +0530

    Version 5: implemented a Abstraction

--- Models/person.py
+++ Models/person.py
@@ -1,17 +1,11 @@
-class Person:
+from abc import ABC, abstractmethod
+
+class Person(ABC):
+
     def __init__(self, name, id):
         self.name = name
-        self.__id = id   # private
+        self._id = id
 
-    def get_id(self):
-        return self.__id
-
-    def set_id(self, new_id):
-        if new_id:
-            self.__id = new_id
-        else:
-            print("Invalid ID")
-
+    @abstractmethod
     def display_details(self):
-        print("Name:", self.name)
-        print("ID:", self.__id)
+        pass
