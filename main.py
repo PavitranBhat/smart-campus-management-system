6e77bea86eb1eb5fee2d5ccee58bda2f4924df94
Pavitra Bhat <pavitranbhat232003@gmail.com>
2026-02-10 19:05:20 +0530

    Version 4: Implemented polymorphism using display_details

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from models.faculty import Faculty
 
 s1 = Student("Pavitra", "S101")
 s1.enroll_course("Python")
-s1.display_details()
-
-print("-------------")
 
 f1 = Faculty("Dr Rao", "F201", "Data Science")
-f1.display_details()
+
+people = [s1, f1]
+
+for person in people:
+    print("------------")
+    person.display_details()
